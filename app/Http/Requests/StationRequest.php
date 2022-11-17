74bfc193bd47d1361a8d17bb77dc404bba7fe109
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-11-17 14:04:36 +0700

    create station request

---
+++ app/Http/Requests/StationRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class StationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check() && Auth::user()->role_id == 2;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:stations',
+            'password' => 'required|string|min:8',
+            'role_id' => 'integer'
+        ];
+    }
+}
