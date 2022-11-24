4042b0851c488b22486a22519eaaa53b6316d7ee
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-11-24 19:07:14 +0700

    fix delete view

--- resources/views/pages/Admin/trains/index.blade.php
+++ resources/views/pages/Admin/trains/index.blade.php
@@ -35,10 +35,10 @@
                                             </td>
                                             <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
                                                 <a href="{{ route('train.edit', $train) }}" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Edit</a>
-                                                <form action="{{ route('train.destroy', $train->id) }}" method="post" class="inline text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                                                <form action="{{ route('train.destroy', $train->id) }}" method="post" class="inline">
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit">Delete</button>
+                                                    <button type="submit" class="inline text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 cursor-pointer">Delete</button>
                                                 </form>
                                             </td>
                                         </tr>
