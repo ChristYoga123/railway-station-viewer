31bc173faacff0e17c11123a25a5ccd4a0f7839b
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-11-27 08:42:19 +0700

    fix index

--- resources/views/pages/Admin/trains/index.blade.php
+++ resources/views/pages/Admin/trains/index.blade.php
@@ -36,21 +36,21 @@
                     </thead>
                     <tbody>
                         @foreach ($trains as $train)
-                        @can('admin')
                             <tr>
                                 <td class="px-8 py-4 whitespace-nowrap text-md text-gray-500">
                                     {{ $train->name }}
                                 </td>
-                                <td class="px-8 py-4 whitespace-nowrap text-md text-gray-500 text-center">
-                                    <a href="{{ route('train.edit', $train) }}" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Edit</a>
-                                    <form action="{{ route('train.destroy', $train->id) }}" method="post" class="inline">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="inline text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 cursor-pointer">Delete</button>
-                                    </form>
-                                </td>
+                                @can('admin')
+                                    <td class="px-8 py-4 whitespace-nowrap text-md text-gray-500 text-center">
+                                        <a href="{{ route('train.edit', $train) }}" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Edit</a>
+                                        <form action="{{ route('train.destroy', $train->id) }}" method="post" class="inline">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="inline text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 cursor-pointer">Delete</button>
+                                        </form>
+                                    </td>
+                                @endcan
                             </tr>
-                        @endcan
                     @endforeach
                     </tbody>
                 </table>
