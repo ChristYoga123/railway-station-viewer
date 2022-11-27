28b0793e11545d323a9d1f6fb5825171a6d26b0a
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-11-27 20:22:41 +0700

    install daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -86,6 +86,7 @@ module.exports = {
 
     plugins: [
         require("@tailwindcss/forms"),
+        require('daisyui'),
         plugin(function ({ addComponents, theme }) {
             const screens = theme("screens", {});
             addComponents([
